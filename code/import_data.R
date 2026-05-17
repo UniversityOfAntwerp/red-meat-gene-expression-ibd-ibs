6777cf00cd229a7b1e1ef28ac59ae1315f94dbf1
UniversityOfAntwerp <v25r26pkhr@privaterelay.appleid.com>
2026-05-17 11:38:26 +0200

    Update import_data.R

--- code/import_data.R
+++ code/import_data.R
@@ -1,1 +1,18 @@
+reactome <- read.table(
+  "data/Ensembl2Reactome_All_Levels.human.txt",
+  sep = "\t",
+  header = FALSE,
+  quote = "",
+  comment.char = "",
+  stringsAsFactors = FALSE
+)
+
+geneiddata <- read_csv(
+  "data/gProfiler_hsapiens_2026-05-06_12-58-07.csv",
+  show_col_types = FALSE
+)
+
+colnames(reactome)[colnames(reactome) == "V1"] <- "gene"
+colnames(reactome)[colnames(reactome) == "V2"] <- "term"
+colnames(reactome)[colnames(reactome) == "V3"] <- "description"
 
