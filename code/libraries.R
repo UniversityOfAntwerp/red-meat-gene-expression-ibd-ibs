0338d93eabe18758c2585941572b67f8017e7b17
UniversityOfAntwerp <v25r26pkhr@privaterelay.appleid.com>
2026-05-17 15:37:08 +0200

    Update libraries.R

--- code/libraries.R
+++ code/libraries.R
@@ -1,13 +1,10 @@
-# List of required packages
 required_packages <- c(
-  "GEOquery",   # GEO data import
-  "Biobase",    # ExpressionSet handling: exprs(), pData()
-  "lme4",       # Linear mixed models
-  "lmerTest",   # p-values for lmer() models
-  "readr"      # Reading/writing files
+  "GEOquery",   
+  "Biobase",    
+  "lme4",      
+  "lmerTest",  
+  "readr"      
 )
 
-# Load packages
 invisible(lapply(required_packages, library, character.only = TRUE))
 
-message("All required libraries loaded successfully.")
